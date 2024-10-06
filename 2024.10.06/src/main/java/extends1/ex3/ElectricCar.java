726ae51003770919ceb6fe7fa54442cdb4b1f868
[SeokHwanJeongKR] <[jsver12@gmail.com]>
2024-10-06 22:35:47 +0900

    Extends, Overriding

---
+++ 2024.10.06/src/main/java/extends1/ex3/ElectricCar.java
@@ -0,0 +1,9 @@
+package extends1.ex3;
+
+public class ElectricCar extends Car {
+
+    static int num;
+    public void charge() {
+        System.out.println("충전 합니다.");
+    }
+}
---
+++ 2024.10.06/src/main/java/extends1/overriding/GasCar.java
@@ -0,0 +1,9 @@
+package extends1.overriding;
+
+public class GasCar extends Car {
+
+
+    public void fillUp() {
+        System.out.println("주유를 합니다");
+    }
+}
